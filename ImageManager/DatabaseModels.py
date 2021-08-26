db857780907ab8f9e26227289d296b0d2f9231b0
HTM-Antonio <ti04@htmeletronica.com.br>
2021-08-26 19:57:30 +0000

    Cusom image manager

---
+++ ImageManager/DatabaseModels.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import re
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from .app import app
+from .utils import HTTPRequestError
+from .conf import CONFIG
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from minio import Minio
+
+app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG.get_db_url()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+minioClient = Minio(CONFIG.s3url, CONFIG.s3user, CONFIG.s3pass, secure=False)
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.String(36), unique=True, nullable=False, primary_key=True)
+    label = db.Column(db.String(128), nullable=False)
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, onupdate=datetime.now)
+    fw_version = db.Column(db.String(128), nullable=False)
+    confirmed = db.Column(db.Boolean, default=False, nullable=False)
+    attrs = db.relationship("Attributes", cascade='all, delete-orphan')
+    def __repr__(self):
+        return "<Image(label={}, fw_version={})>".format(self.label, self.fw_version)
+
+class Attributes(db.Model):
+    __tablename__ = 'attributes'
+    
+    id = db.Column(db.String(36), unique=True, nullable=False, primary_key=True)
+    label = db.Column(db.String(128), nullable=False)
+    value = db.Column(db.String(128), nullable=True)
+    image_id = db.Column(db.String(36), db.ForeignKey('images.id'))
+
+
+def assert_image_exists(image_id):
+    try:
+        return Image.query.filter_by(id=image_id).one()
+    except sqlalchemy.orm.exc.NoResultFound:
+        raise HTTPRequestError(404, "No such image: %s" % image_id)
+
+
+def get_all_images():
+    return Image.query.all()
+
+
+def get_all_images_filter(label):
+    try:
+        return Image.query.filter_by(**label).all()
+    except InvalidRequestError:
+        raise HTTPRequestError(400, 'Invalid query param supplied')
+
+
+def handle_consistency_exception(error):
+    # message = error.message.replace('\n','')
+    message = re.sub(r"(^\(.*?\))|\n", "", error.message)
+    raise HTTPRequestError(400, message)
---
+++ docs/using-image-manager.md
@@ -0,0 +1,37 @@
+# Using ImageManager
+
+These instructions are meant for development only. 
+They should be run on the repository root and will execute the following:
+1. Run the necessary environment(postgres+minio) using docker-compose
+2. Build an image based on the current repository
+3. Run the application inside docker compose network
+4. Run the test application
+
+Obs.: Using the option  ```-v $PWD:/usr/src/app local/imagemanager```
+will link the docker image application with the local development code,
+any changes in the code will update the application immediately using flask's hot
+swap mechanism
+
+On terminal #1:
+
+    docker-compose -f local/compose.yml -p imgm up -d
+    docker build -f Dockerfile -t local/imagemanager .
+    docker run --rm -it --network imgm_default -p 8000:5000 --network-alias image-manager -v $PWD:/usr/src/app local/imagemanager
+    
+On terminal #2:
+    
+    cd tests/
+    python3 client.py
+
+
+
+Request samples using authorization token as {"service": "mytenant"}:
+
+    curl http://localhost:8000/image -H "Accept: application/json" -H "Authorization: Bearer my.eyJzZXJ2aWNlIjogIm15dGVuYW50In0="
+
+
+    curl -X POST http://localhost:8000/image/ -H "Accept: application/json" -H "Authorization: Bearer my.eyJzZXJ2aWNlIjogIm15dGVuYW50In0=" -H "Content-Type: application/json"  -d '{"label": "FW_Example","fw_version": "1.0.0", "attrs": [{"label": "mandatory","value": "true"},{"label": "description","value": "description"}]}'
+
+
+    curl -X DELETE http://localhost:8000/image/<image_id> -H "Accept: application/json" -H "Authorization: Bearer my.eyJzZXJ2aWNlIjogIm15dGVuYW50In0="
+ 
